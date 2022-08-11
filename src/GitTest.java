1e274d3561ab4e59096bd90e0acdf346ab6e17f5
cxy <cxy@cxy.cxy>
2022-08-11 08:54:48 +0800

    切换测试

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hot-fix");
         System.out.println("master-commit1");
+        System.out.println("切换测试");
     }
 }
